a53b55be41d0f3717d52e31f4fd660e41fb4af62
robertvi <robert.vickerstaff@emr.ac.uk>
2015-05-01 11:43:33 +0100

    modified exercise

--- workshop008/exercise.py
+++ workshop008/exercise.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
 '''
-(1) as an exercise, take a list of random integers and sort them into order
-using your own code
+(1) as an exercise, take a list of random integers and sort them into ascending
+order using your own code
 if you cannot think of a way to sort the list into order using python that
 you know, do an internet search for "bubble sort algorithm"
 the code below gets you started with a list of random unsorted numbers
 (2) make a copy of the unsorted list and sort it using the builtin .sort() function
-then compare element by element to check that the two lists are identical
-
-
+then write code to compare element by element to check that the two lists are identical
+(3) run the script several times to check that:
+(a) the starting random numbers list produced by the randint function is
+different each time and
+(b) your sorting method always produces the same order as the built in .sort method
 '''
 
 #exercise: sort a list of random integers by swapping elements
